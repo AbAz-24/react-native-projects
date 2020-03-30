415caf09930a93b40b29db9ab38a6e1c04518c69
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-03-31 01:40:45 +0700

    Componets level state

--- albums/index.js
+++ albums/index.js
@@ -1,12 +1,15 @@
-/* eslint-disable prettier/prettier */
 // Import a library to help create a component
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, View} from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a componet
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
