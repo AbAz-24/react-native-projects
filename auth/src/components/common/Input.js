4d11d1de83b6cdd43d3b35184827c12c1f452f24
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-03 07:32:52 +0700

    Wrapping up Inputs

--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {TextInput, View, Text} from 'react-native';
 
-const Input = ({label, value, onChangeText}) => {
+const Input = ({label, value, onChangeText, placeHolder}) => {
   const {inputStyle, labelStyle, containerStyle} = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
-      <TextInput style={inputStyle} value={value} onChangeText={onChangeText} />
+      <TextInput
+        placeholder={placeHolder}
+        autoCorrect={false}
+        style={inputStyle}
+        value={value}
+        onChangeText={onChangeText}
+      />
     </View>
   );
 };
@@ -19,7 +25,7 @@ const styles = {
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23,
-    flex: 2,
+    flex: 3,
   },
   labelStyle: {
     fontSize: 18,
