f4a33e17e5ccd044cf815ebb07051ea2a567d67f
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-07 02:54:45 +0700

    Wiring up action creators

--- manager/src/reducers/index.js
+++ manager/src/reducers/index.js
@@ -1,3 +1,4 @@
 import {combineReducers} from 'redux';
+import AuthReducer from './AuthReducer';
 
-export default combineReducers({banana: () => []});
+export default combineReducers({auth: AuthReducer});
