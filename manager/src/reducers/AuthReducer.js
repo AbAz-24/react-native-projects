cc8e7a41de36ee041ec531f1ee11c045740db736
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-08 00:02:50 +0700

    A firebase gotcha

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -13,17 +13,15 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
-
   switch (action.type) {
     case EMAIL_CHANGED:
       return {...state, email: action.payload};
     case PASSWORD_CHANGED:
       return {...state, password: action.payload};
     case LOGIN_USER_SUCCESS:
-      return {...state, user: action.payload};
+      return {...state, user: action.payload, error: ''};
     case LOGIN_USER_FAIL:
-      return {...state, error: 'Authentication Failed.'};
+      return {...state, error: 'Authentication Failed.', password: ''};
     default:
       return state;
   }
