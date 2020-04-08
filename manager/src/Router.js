6290b858bab75282aed70a8af1d014f96b8325b0
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-09 00:15:18 +0700

    Navigation bar buttons

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -18,6 +18,10 @@ const RouterComponent = () => {
         </Scene>
         <Scene key="main">
           <Scene
+            rightTitle="Add"
+            onRight={() => {
+              console.log('right!');
+            }}
             key="employeeList"
             component={EmployeeList}
             title="Employee List"
