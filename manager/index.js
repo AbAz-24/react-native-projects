fb269c3b7a9f4f66280a8362448dba198843f3c1
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-10 03:09:19 +0700

    Successful data save to firebase

--- manager/index.js
+++ manager/index.js
@@ -1,4 +1,6 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/App';
 
 AppRegistry.registerComponent('manager', () => App);
+
+YellowBox.ignoreWarnings(['Remote debugger', 'Setting a timer']);
