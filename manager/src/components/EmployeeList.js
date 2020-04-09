28c51f921f569cd0fd4222bb074acb059c8f67c8
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-10 06:42:07 +0700

    List Building in Employee List

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,44 +1,22 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {View, Text} from 'react-native';
-import {ListView} from 'deprecated-react-native-listview';
+import {FlatList} from 'react-native';
 import {employeesFetch} from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentDidMount() {
     this.props.employeesFetch();
-
-    this.createDataSource(this.props);
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    // nextProps are the next set of props that this component
-    // will be render with
-    // this.props is still the old set of props
-
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({employees}) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-    });
-
-    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
-    console.log(this.props);
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        renderItem={employee => <ListItem employee={employee.item} />}
+        keyExtractor={employee => employee.uid}
+      />
     );
   }
 }
