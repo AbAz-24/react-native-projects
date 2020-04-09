d6c68df9111b07e474ba3cb888da3f4c92a60e01
Abdullah_Azzam <abdullahazzam24@gmail.com>
2020-04-10 05:46:23 +0700

    Transforming objects to arrays

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {ListView, View, Text} from 'react-native';
+import {View, Text} from 'react-native';
+import {ListView} from 'deprecated-react-native-listview';
 import {employeesFetch} from '../actions';
 
 class EmployeeList extends Component {
@@ -10,7 +12,7 @@ class EmployeeList extends Component {
     this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     // nextProps are the next set of props that this component
     // will be render with
     // this.props is still the old set of props
@@ -27,6 +29,7 @@ class EmployeeList extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Employee List</Text>
@@ -40,7 +43,15 @@ class EmployeeList extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return {...val, uid}; //{shift: 'monday', name: 'jane', id: '1j2j3j'}
+  });
+
+  return {employees};
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   {employeesFetch},
 )(EmployeeList);
